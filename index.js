e1713b9a9e6e603b85eeab0b3bd3b08c5fd4d23a
Rishabh Raghav <rishabhraghav@Rishabhs-MacBook-Air.local>
2021-09-13 21:21:12 +0530

    portfolio in localhost

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/api', (req, res) => {
+    res.send('API status: Running');
+    
+})
+app.post('/api', (req, res) => {
+    console.log('connected');
+
+    
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        // service: 'gmail',
+        auth: {
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.PASSWORD, // generated ethereal password
+        },
+      });
+    
+      // send mail with defined transport object
+      const info = {
+        from: 'rishabhraghav2012@gmail.com', // sender address
+        to: `rishabhraghav2012@gmail.com`, // list of receivers
+        subject: `Message from ${req.body.form.name}`, // Subject line
+        text: `${req.body.form.text} (Reach me out @ ${req.body.form.email})`, // plain text body
+      };
+
+      transporter.sendMail(info, (err, data) => {
+            if(err) {
+                console.log(`error occurs ${err}`);
+            } else {
+                console.log(`email sent!!!  ${data.response}`);
+            }
+      });
+
+});
+
+
+app.listen(3030, '0.0.0.0', (req, res) => {
+    console.log('node server started!');
+});
